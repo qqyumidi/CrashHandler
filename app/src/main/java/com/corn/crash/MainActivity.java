b54b32543de615e6d5a3bb698bc78725b62ee0d9
corn <103069346@qq.com>
2019-11-27 20:09:55 +0800

    未捕获异常处理示例

--- app/src/main/java/com/corn/crash/MainActivity.java
+++ app/src/main/java/com/corn/crash/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +28,68 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                logUncaughtExceptionHandler();
+
+                final Thread.UncaughtExceptionHandler defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+                Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                for (int i = 0; i < 101190000; i++ ) {
+                                    if(i%10000 == 0) {
+                                        System.out.println("thread name:" + Thread.currentThread().getName() + " xxxx-i:" + i);
+                                    }
+                                }
+                            }
+                        }).start();
+
+                        try {
+                            Thread.sleep(3000);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                        defaultUncaughtExceptionHandler.uncaughtException(t, e);
+
+                    }
+                });
+                errorMethod();
+
+
+                System.out.println("thread name:" + Thread.currentThread().getName() + "come here");
+                Thread thread = new Thread(new MyRunnable());
+                //thread.start();
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public static void logUncaughtExceptionHandler(){
+        Thread thread = Thread.currentThread();
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = thread.getUncaughtExceptionHandler();
+        Thread.UncaughtExceptionHandler  defaultUncaughtExceptionHandler = thread.getDefaultUncaughtExceptionHandler();
+
+        System.out.println("thread name:" + thread.getName() + " uncaughtExceptionHandler:" + uncaughtExceptionHandler
+                + " defaultUncaughtExceptionHandler:" + defaultUncaughtExceptionHandler);
+
+        try {
+            Class threadClass = Class.forName("java.lang.Thread");
+            Method method = threadClass.getDeclaredMethod("getUncaughtExceptionPreHandler", null);
+            method.setAccessible(true);
+            Object obj = method.invoke(threadClass, null);
+
+            System.out.println("thread name:" + thread.getName() + " pre:" + (Thread.UncaughtExceptionHandler)obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 
     @Override
@@ -51,5 +112,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("thread name:" + Thread.currentThread().getName() + " start run");
+
+            errorMethod();
+
+            System.out.println("thread name:" + Thread.currentThread().getName() + " end run");
+        }
+    }
+
+    public static int errorMethod() {
+        String name = null;
+
+        return name.length();
     }
 }
--- javaLib/src/main/java/com/corn/javalib/MyClass.java
+++ javaLib/src/main/java/com/corn/javalib/MyClass.java
@@ -1,4 +1,62 @@
 package com.corn.javalib;
 
 public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("thread name:" + Thread.currentThread().getName() + " begin...");
+        logUncaughtExceptionHandler();
+
+
+        Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                System.out.println("thread name:" + Thread.currentThread().getName() + " uncaughtException...");
+            }
+        });
+
+        //errorMethod();
+
+        Thread thread = new Thread(new MyRunnable());
+        thread.start();
+
+
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("thread name:" + Thread.currentThread().getName() + " end...");
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("thread name:" + Thread.currentThread().getName() + " start run");
+            logUncaughtExceptionHandler();
+
+            errorMethod();
+
+            System.out.println("thread name:" + Thread.currentThread().getName() + " end run");
+        }
+    }
+
+    public static int errorMethod() {
+        String name = null;
+
+        return name.length();
+    }
+
+
+    public static void logUncaughtExceptionHandler(){
+        Thread thread = Thread.currentThread();
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = thread.getUncaughtExceptionHandler();
+        Thread.UncaughtExceptionHandler  defaultUncaughtExceptionHandler = thread.getDefaultUncaughtExceptionHandler();
+
+        System.out.println("thread name:" + thread.getName() + " uncaughtExceptionHandler:" + uncaughtExceptionHandler
+                            + " defaultUncaughtExceptionHandler:" + defaultUncaughtExceptionHandler);
+
+
+    }
 }
